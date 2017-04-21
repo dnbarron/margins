ae7805c5b1a00553b454359017fe83137440a217
Thomas J. Leeper <thosjleeper@gmail.com>
2017-04-21 14:53:32 +0100

    finish cplot.clm (closes #63)

--- R/marginal_effects_nnet.R
+++ R/marginal_effects_nnet.R
@@ -12,13 +12,16 @@ function(model,
     
     # estimate numerical derivatives with respect to each variable (for numeric terms in the model)
     # add discrete differences for logical terms
-    out1 <- lapply(c(vars$nnames, vars$lnames), dydx, data = data, model = model, type = NULL, eps = eps, ...)
+    out1 <- lapply(c(vars$nnames, vars$lnames), dydx, data = data, model = model, 
+                   type = NULL, eps = eps, ...)
     
     # add discrete differences for factor terms
     ## exact number depends on number of factor levels
     out2 <- list()
     for (i in seq_along(vars$fnames)) {
-        out2[[i]] <- dydx.factor(data = data, model = model, vars$fnames[i], fwrap = (vars$fnames != vars$fnames2)[i], type = NULL, ...)
+        out2[[i]] <- dydx.factor(data = data, model = model, vars$fnames[i], 
+                                 fwrap = (vars$fnames != vars$fnames2)[i], 
+                                 type = NULL, ...)
     }
     
     out <- c(out1, out2)
